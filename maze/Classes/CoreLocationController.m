dae98d980593c1f30e0d44e2ca96ec015504f59c
Daniel Ortiz <IndolentSpaniard@gmail.com>
2009-10-03 14:23:30 -0400

    Add the maze.

---
+++ maze/Classes/CoreLocationController.m
@@ -0,0 +1,34 @@
+//
+//  CoreLocationController.m
+//  ExtendedRealityGames
+//
+//  Created by Daniel  Ortiz on 10/2/09.
+//
+
+#import "CoreLocationController.h"
+
+
+@implementation CoreLocationController
+
+@synthesize locationManager;
+@synthesize delegate;
+
+- (id) init {
+	self = [super init];
+	if(self != nil){
+		self.locationManager = [[[CLLocationManager alloc] init] autorelease];
+		self.locationManager.delegate = self;		/* Send location data to this controller */
+	}
+}
+
+-(void)locationManager:(CLLocationManager*)manager
+   didUpdateToLocation:(CLLocation*)newLocation
+		  fromLocation:(CLLocation*)oldLocation{
+	[self.delegate locationUpdate:newLocation oldLocation:oldLocation];
+}
+
+- (void) locationManager:(CLLocationManager*)manager
+		didFailWithError:(NSError*)error{
+	[self.delegate locationError:error];
+}
+@end
